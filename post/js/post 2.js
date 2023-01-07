c1a12167a5208854dfdc4690241efa9a5af86e8d
moepetqqa <me4571570@gmail.com>
2023-01-07 20:09:00 +0800

    1.1.1

---
+++ post/js/post 2.js
@@ -0,0 +1,27 @@
+$(".hd").click(function () {
+    $(".hd-board").toggleClass("hd-display");
+  });
+
+
+$(document).ready(function() {
+    $(".stick-1").click(function() {
+    $(".hd-stick").attr("src", "./img/stick/stick-1.png");});
+
+    $(".stick-2").click(function() {
+    $(".hd-stick").attr("src", "./img/stick/stick-2.png");});
+
+    $(".stick-3").click(function() {
+    $(".hd-stick").attr("src", "./img/stick/stick-3.png");});
+
+    $(".stick-4").click(function() {
+    $(".hd-stick").attr("src", "./img/stick/stick-4.png");});
+
+    $(".stick-5").click(function() {
+    $(".hd-stick").attr("src", "./img/stick/stick-5.png");});
+
+    $(".stick-6").click(function() {
+    $(".hd-stick").attr("src", "./img/stick/stick-6.png");});
+    
+    $(".stick-7").click(function() {
+    $(".hd-stick").attr("src", "./img/stick/stick-7.png");});
+  });
